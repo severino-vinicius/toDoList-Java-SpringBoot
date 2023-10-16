216069c2f10dedbdaf1801f2bfd466031febdf3e
Severino Vinicius <severino.sales95@gmail.com>
2023-10-16 17:47:37 -0300

    [Criando ToDoList] - Api Usando Java e Spring Boot

---
+++ src/main/java/br/com/viniciusjava/todolist/user/UserRepository.java
@@ -0,0 +1,10 @@
+package br.com.viniciusjava.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<UserModel, UUID>{
+  UserModel findByUsername(String username);
+}
---
+++ src/test/java/br/com/viniciusjava/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.viniciusjava.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
